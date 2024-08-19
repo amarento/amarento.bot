f9c68810fe26a14f03076dbe8b7ed069d242c242
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-08-19 21:42:56 +0200

    🥩 Update database types

--- wa-bot-ts/database.types.ts
+++ wa-bot-ts/database.types.ts
@@ -1,181 +1,317 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export type Database = {
   public: {
     Tables: {
-      countries: {
+      "amarento.id_account": {
         Row: {
-          created_at: string
-          id: number
-          name: string | null
-        }
+          access_token: string | null;
+          expires_at: number | null;
+          id_token: string | null;
+          provider: string;
+          provider_account_id: string;
+          refresh_token: string | null;
+          scope: string | null;
+          session_state: string | null;
+          token_type: string | null;
+          type: string;
+          user_id: string;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-          name?: string | null
-        }
+          access_token?: string | null;
+          expires_at?: number | null;
+          id_token?: string | null;
+          provider: string;
+          provider_account_id: string;
+          refresh_token?: string | null;
+          scope?: string | null;
+          session_state?: string | null;
+          token_type?: string | null;
+          type: string;
+          user_id: string;
+        };
         Update: {
-          created_at?: string
-          id?: number
-          name?: string | null
-        }
-        Relationships: []
-      }
-      guests: {
+          access_token?: string | null;
+          expires_at?: number | null;
+          id_token?: string | null;
+          provider?: string;
+          provider_account_id?: string;
+          refresh_token?: string | null;
+          scope?: string | null;
+          session_state?: string | null;
+          token_type?: string | null;
+          type?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "amarento.id_account_user_id_amarento.id_user_id_fk";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "amarento.id_user";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      "amarento.id_clients": {
         Row: {
-          address: string
-          created_at: string
-          guest_names: Json | null
-          id: number
-          inv_names: string
-          n_rsvp_dinner_act: number
-          n_rsvp_dinner_wa: number
-          n_rsvp_holmat_act: number
-          n_rsvp_holmat_wa: number
-          n_rsvp_plan: number
-          rsvp_afpat: boolean
-          rsvp_dinner: boolean
-          rsvp_holmat: boolean
-          updated_at: string | null
-          wa_number: string
-        }
+          client_code: string;
+          created_at: string;
+          dinner_location: string | null;
+          dinner_time: string | null;
+          holmat_location: string | null;
+          holmat_time: string | null;
+          id: number;
+          name_bride: string | null;
+          name_groom: string | null;
+          parents_name_bride: string | null;
+          parents_name_groom: string | null;
+          updated_at: string | null;
+          wedding_day: string | null;
+        };
         Insert: {
-          address: string
-          created_at?: string
-          guest_names?: Json | null
-          id?: number
-          inv_names: string
-          n_rsvp_dinner_act?: number
-          n_rsvp_dinner_wa?: number
-          n_rsvp_holmat_act: number
-          n_rsvp_holmat_wa?: number
-          n_rsvp_plan: number
-          rsvp_afpat?: boolean
-          rsvp_dinner?: boolean
-          rsvp_holmat?: boolean
-          updated_at?: string | null
-          wa_number: string
-        }
+          client_code: string;
+          created_at?: string;
+          dinner_location?: string | null;
+          dinner_time?: string | null;
+          holmat_location?: string | null;
+          holmat_time?: string | null;
+          id?: number;
+          name_bride?: string | null;
+          name_groom?: string | null;
+          parents_name_bride?: string | null;
+          parents_name_groom?: string | null;
+          updated_at?: string | null;
+          wedding_day?: string | null;
+        };
         Update: {
-          address?: string
-          created_at?: string
-          guest_names?: Json | null
-          id?: number
-          inv_names?: string
-          n_rsvp_dinner_act?: number
-          n_rsvp_dinner_wa?: number
-          n_rsvp_holmat_act?: number
-          n_rsvp_holmat_wa?: number
-          n_rsvp_plan?: number
-          rsvp_afpat?: boolean
-          rsvp_dinner?: boolean
-          rsvp_holmat?: boolean
-          updated_at?: string | null
-          wa_number?: string
-        }
-        Relationships: []
-      }
-    }
+          client_code?: string;
+          created_at?: string;
+          dinner_location?: string | null;
+          dinner_time?: string | null;
+          holmat_location?: string | null;
+          holmat_time?: string | null;
+          id?: number;
+          name_bride?: string | null;
+          name_groom?: string | null;
+          parents_name_bride?: string | null;
+          parents_name_groom?: string | null;
+          updated_at?: string | null;
+          wedding_day?: string | null;
+        };
+        Relationships: [];
+      };
+      "amarento.id_guests": {
+        Row: {
+          client_id: number;
+          created_at: string;
+          guest_names: string | null;
+          id: number;
+          inv_names: string;
+          n_rsvp_dinner_act: number | null;
+          n_rsvp_dinner_wa: number | null;
+          n_rsvp_holmat_act: number | null;
+          n_rsvp_holmat_wa: number | null;
+          n_rsvp_plan: number;
+          rsvp_dinner: boolean | null;
+          rsvp_holmat: boolean | null;
+          updated_at: string | null;
+          wa_number: string;
+        };
+        Insert: {
+          client_id: number;
+          created_at?: string;
+          guest_names?: string | null;
+          id?: number;
+          inv_names: string;
+          n_rsvp_dinner_act?: number | null;
+          n_rsvp_dinner_wa?: number | null;
+          n_rsvp_holmat_act?: number | null;
+          n_rsvp_holmat_wa?: number | null;
+          n_rsvp_plan: number;
+          rsvp_dinner?: boolean | null;
+          rsvp_holmat?: boolean | null;
+          updated_at?: string | null;
+          wa_number: string;
+        };
+        Update: {
+          client_id?: number;
+          created_at?: string;
+          guest_names?: string | null;
+          id?: number;
+          inv_names?: string;
+          n_rsvp_dinner_act?: number | null;
+          n_rsvp_dinner_wa?: number | null;
+          n_rsvp_holmat_act?: number | null;
+          n_rsvp_holmat_wa?: number | null;
+          n_rsvp_plan?: number;
+          rsvp_dinner?: boolean | null;
+          rsvp_holmat?: boolean | null;
+          updated_at?: string | null;
+          wa_number?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "amarento.id_guests_client_id_amarento.id_clients_id_fk";
+            columns: ["client_id"];
+            isOneToOne: false;
+            referencedRelation: "amarento.id_clients";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      "amarento.id_session": {
+        Row: {
+          expires: string;
+          session_token: string;
+          user_id: string;
+        };
+        Insert: {
+          expires: string;
+          session_token: string;
+          user_id: string;
+        };
+        Update: {
+          expires?: string;
+          session_token?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "amarento.id_session_user_id_amarento.id_user_id_fk";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "amarento.id_user";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      "amarento.id_user": {
+        Row: {
+          email: string;
+          email_verified: string | null;
+          id: string;
+          image: string | null;
+          name: string | null;
+        };
+        Insert: {
+          email: string;
+          email_verified?: string | null;
+          id: string;
+          image?: string | null;
+          name?: string | null;
+        };
+        Update: {
+          email?: string;
+          email_verified?: string | null;
+          id?: string;
+          image?: string | null;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
+      "amarento.id_verification_token": {
+        Row: {
+          expires: string;
+          identifier: string;
+          token: string;
+        };
+        Insert: {
+          expires: string;
+          identifier: string;
+          token: string;
+        };
+        Update: {
+          expires?: string;
+          identifier?: string;
+          token?: string;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"]) | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+  ? (PublicSchema["Tables"] & PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database },
+  PublicEnumNameOrOptions extends keyof PublicSchema["Enums"] | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
-
+  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+  : never;
